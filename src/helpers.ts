818e426fc2f0ebb030fbc926b829a784f8422e3f
sanyanemesh <sanya.nemesh@gmail.com>
2024-04-19 12:36:10 +0300

    removed hours and seconds from date buttons

--- src/helpers.ts
+++ src/helpers.ts
@@ -40,9 +40,7 @@ export function convertTimestampToDateString(timestamp: number| null): string {
   const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed, so add 1
   const day = String(date.getDate()).padStart(2, '0');
   const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  return `${year}-${month}-${day} ${hours}`;
 }
 
 export function formatTime(seconds: number): string {
